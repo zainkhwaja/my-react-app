47a7014df4532e4ffc71600a92c7c8632372e3d8
zain jawwad <zain@sudofy.com>
2018-05-11 10:04:31 +0500

    Burger checkout modal and backdrop

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import classes from './BuildControl.css';
-const buildControl = (props) => (
+import classes from "./BuildControl.css";
+const buildControl = props => (
   <div className={classes.BuildControl}>
-    <div>{props.label}</div>      
-    <button>Less</button>
-    <button>More</button>
+    <div>{props.label}</div>
+    <button disabled={props.disabled} onClick={props.removed}>
+      Less
+    </button>
+    <button onClick={props.added}>More</button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,10 +11,16 @@ const controls = [
 ];
 
 const buildControls = (props) => (
-  <div className={classes.buildControls}>
+  <div className={classes.BuildControls}>
+  <p>Current Price: <strong>{props.price.toFixed(2)}</strong> </p>
   {controls.map(ctrl => (
-    <BuildControl key={ctrl.label} label={ctrl.label} />
+    <BuildControl key={ctrl.label} label={ctrl.label}
+     added={() => props.ingredientAdded(ctrl.type)} 
+    removed={() => props.ingredientRemoved(ctrl.type)}
+    disabled={props.disabled[ctrl.type]} />
   ))}
+
+  <button onClick={props.ordered} disabled={!props.purchasable} className={classes.OrderButton}>ORDER NOW</button>
   </div>
 
 );
